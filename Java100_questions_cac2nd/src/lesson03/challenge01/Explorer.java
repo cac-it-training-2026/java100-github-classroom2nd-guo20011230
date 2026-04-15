12d236f814e4d11045ae13de1d5b4808cbc4e672
yupuguo <yupuguo@C010423081.zipangu.cac.co.jp>
2026-04-15 15:30:16 +0900

    lesson03challenge04完了

--- Java100_questions_cac2nd/src/lesson03/challenge01/Explorer.java
+++ Java100_questions_cac2nd/src/lesson03/challenge01/Explorer.java
@@ -1,36 +1,3 @@
-/**
- * 第3章 探検隊のお仕事
- *
- * 問題1  探検隊の募集
- *
- * コメントの位置にfor文を記述して
- * 実行例と同じメッセージを表示してください。
- *
- * <実行例>
- *  隊長：
- *  探検隊の隊員を5名募集します。
- *
- *  応募する人の名前を入れてください＞○○
- *
- *  隊長：
- *  ○○さん合格！
- *
- *  応募する人の名前を入れてください＞△△
- *
- *  隊長：
- *  △△さん合格！
- *
- *  ～～～～～～～～ 中略 ～～～～～～～～～
- *
- *  応募する人の名前を入れてください＞□□
- *
- *  隊長：
- *  □□さん合格！
- *
- *  定員に達しました。募集を締め切ります。
- *
- */
-
 package lesson03.challenge01;
 
 import java.io.BufferedReader;
@@ -46,17 +13,13 @@ public class Explorer {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		//ここにfor文を記述する。
+		for (int i = 0; i < 5; i++) {
 
 			System.out.print("応募する人の名前を入れてください＞");
 
 			String name = br.readLine();
 			System.out.println("\n隊長：");
 			System.out.println(name + "さん合格！\n");
-
-		//ここに  } を記述する。
+		}
 
 		System.out.println("定員に達しました。募集を締め切ります。");
-
-	}
-}
